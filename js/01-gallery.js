980459ddfe63e5919b51ef8f908b22658b361330
PolinaFaustova <polinamusikerin@gmail.com>
2023-03-03 21:52:42 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+// Создание и рендер разметки по массиву данных
+// galleryItems и предоставленному шаблону элемента галереи.
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createCardsGallery(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+const cardsGallery = createCardsGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+
+const galleryLinks = document.querySelectorAll("a.gallery__link");
+
+galleryLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+});
